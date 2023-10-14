24aa51e0f81709f6f653aa48201cfa4685a56156
sami <msami.bese22seecs@student.nust.edu.pk>
2023-10-14 20:21:49 +0500

    REst Api

--- start.go
+++ start.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Portfolio struct {
+	PID           string `json:"PID"`
+	PortfolioName string `json:"portfolioname"`
+	annualprofit  string `json:"profit"`
+}
+
+var portfs = []Portfolio{
+	{PID: "1", PortfolioName: "Tech Top 10 Companies", annualprofit: "34"},
+	{PID: "2", PortfolioName: "Agriculture", annualprofit: "12"},
+}
+
+func getportf(c *gin.Context) {
+
+	c.IndentedJSON(http.StatusOK, portfs)
+
+}
+
 func main() {
 
+	router := gin.Default()
+	router.GET("/books", getportf)
+	router.Run("localhost:7070")
 }
