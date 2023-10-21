b7187d49427cbb39d0d10390601574842695e66a
sami <msami.bese22seecs@student.nust.edu.pk>
2023-10-21 21:59:59 +0500

    testing

--- start.go
+++ start.go
@@ -15,17 +15,23 @@ type Portfolio struct {
 var portfs = []Portfolio{
 	{PID: "1", PortfolioName: "Tech Top 10 Companies", annualprofit: "34"},
 	{PID: "2", PortfolioName: "Agriculture", annualprofit: "12"},
+	{PID: "3", PortfolioName: "Energy", annualprofit: "13"},
 }
 
+//Get Data Functions
 func getportf(c *gin.Context) {
 
 	c.IndentedJSON(http.StatusOK, portfs)
 
 }
 
+//Main Function
+
 func main() {
 
 	router := gin.Default()
+	//router.GET("/",print101)
 	router.GET("/books", getportf)
-	router.Run("localhost:7070")
+	router.Run("localhost:7080")
+
 }
